a49dfc9f1145cc80b4ed60ac9b36d3ed024361f7
jcanizales <jcanizales@asesoftware.com>
2021-06-18 09:12:24 -0500

    Implementación para la consulta de Comercios mostrando sus servicios (FK)

--- turnos/src/main/java/com/asesoftware/semilla/turnos/entity/ComerciosEntity.java
+++ turnos/src/main/java/com/asesoftware/semilla/turnos/entity/ComerciosEntity.java
@@ -1,10 +1,13 @@
 package com.asesoftware.semilla.turnos.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -25,5 +28,12 @@ public class ComerciosEntity {
 	
 	@Column(name = "aforo")
 	private Integer aforo;
+	
+	@Column(name = "nombre")
+	private String nombre;
+	
+	@OneToMany(mappedBy = "id_comercio")
+	private List<ServiciosEntity> servicios;
+	
 }
 
